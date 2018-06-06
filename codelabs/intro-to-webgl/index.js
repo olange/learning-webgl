10913c6d024381c013cda1a216f03a7f9778be3c
Olivier Lange <olange@petit-atelier.ch>
2018-06-07 01:55:35 +0200

    Adds two cubes to the scene

--- codelabs/intro-to-webgl/index.js
+++ codelabs/intro-to-webgl/index.js
@@ -1,27 +1,36 @@
 "use strict";
 
 import {
-  Scene, PerspectiveCamera, WebGLRenderer,
-  BoxGeometry, MeshNormalMaterial, Mesh } from "./node_modules/three/build/three.module.js";
+    Scene, PerspectiveCamera, WebGLRenderer,
+    BoxGeometry, MeshNormalMaterial, Mesh
+  } from "./node_modules/three/build/three.module.js";
 
 const scene = new Scene();
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new PerspectiveCamera( 75, aspect, 0.1, 1000);
-var renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement);
 
 const geometry = new BoxGeometry( 1, 1, 1);
 const material = new MeshNormalMaterial();
-const cube = new Mesh( geometry, material);
-scene.add( cube);
+const cube1 = new Mesh( geometry, material);
+const cube2 = new Mesh( geometry, material);
+const cube3 = new Mesh( geometry, material);
+scene.add( cube1);
+scene.add( cube2);
+scene.add( cube3);
 camera.position.z = 5;
 
 function update( time) {
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
-  cube.position.x = 2.5 * Math.cos( time / 1500.0);
-  cube.position.y = 2.5 * Math.sin( time / 1500.0);
+  cube1.rotation.x = cube2.rotation.x = cube3.rotation.x += 0.1;
+  cube1.rotation.y = cube2.rotation.y = cube3.rotation.y += 0.1;
+  cube1.position.x = 2.5 * Math.cos( time / 1500.0);
+  cube1.position.y = 2.5 * Math.sin( time / 1500.0);
+  cube2.position.x = 2.5 * Math.cos( time * 0.66 * Math.PI / 1500.0);
+  cube2.position.y = 2.5 * Math.sin( time * 0.66 * Math.PI / 1500.0);
+  cube3.position.x = 2.5 * Math.cos( time * 1.33 * Math.PI / 1500.0);
+  cube3.position.y = 2.5 * Math.sin( time * 1.33 * Math.PI / 1500.0);
 }
 
 let lasttime = undefined;
