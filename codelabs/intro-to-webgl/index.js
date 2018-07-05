6c3a14c84428c50033dbf5966ae98d3284af906f
Olivier Lange <olange@petit-atelier.ch>
2018-07-05 15:29:00 +0200

    Adds earth spherical texture color map to sphere mesh

--- codelabs/intro-to-webgl/index.js
+++ codelabs/intro-to-webgl/index.js
@@ -3,11 +3,12 @@
 import {
     WebGLRenderer,
     Scene, PerspectiveCamera, 
-    Geometry, BoxGeometry,
-    Mesh, MeshNormalMaterial,
+    Geometry, BoxGeometry, SphereGeometry,
+    Mesh, MeshNormalMaterial, MeshPhongMaterial, MeshBasicMaterial,
     Line, LineDashedMaterial,
+    AmbientLight,
     Vector3,
-    SphereGeometry
+    TextureLoader
   } from "./node_modules/three/build/three.module.js";
 
 import OrbitControls from "./node_modules/orbit-controls-es6/src/index.js";
@@ -19,21 +20,33 @@ const clientHeight = container.clientHeight;
 const scene = new Scene();
 const aspect = clientWidth / clientHeight;
 const camera = new PerspectiveCamera( 75, aspect, 0.1, 1000);
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer( { antialias: true });
 
 const canvas = renderer.domElement;
 renderer.setSize( clientWidth, clientHeight);
 container.appendChild( canvas);
 
+const light = new AmbientLight( 0xFFFFFF); // soft white light
+scene.add( light);
+
 const boxGeometry = new BoxGeometry( 1, 1, 1);
-const sphereGeometry = new SphereGeometry( 1, 24, 3);
+const sphereGeometry = new SphereGeometry( 1, 24, 16);
+
+const loader = new TextureLoader();
+const earthTexture = loader.load( "assets/textures/earth_atmos_2048.jpg");
 const normalMaterial = new MeshNormalMaterial();
-const wireframeMaterial = new MeshNormalMaterial();
-wireframeMaterial.wireframe = true;
+const earthMaterial = new MeshPhongMaterial( {
+  color: 0xFFFFFF,
+  specular: 0x333333,
+  shininess: 15.0,
+  map: earthTexture
+});
+
 const box1 = new Mesh( boxGeometry, normalMaterial);
 const box2 = new Mesh( boxGeometry, normalMaterial);
 const box3 = new Mesh( boxGeometry, normalMaterial);
-const sphere = new Mesh( sphereGeometry, wireframeMaterial)
+const sphere = new Mesh( sphereGeometry, earthMaterial)
+sphere.rotation.x = Math.PI / 2.0;
 scene.add( box1);
 scene.add( box2);
 scene.add( box3);
@@ -63,8 +76,7 @@ controls.enableZoom = true;
 function update( time) {
   box1.rotation.x = box2.rotation.x = box3.rotation.x += 0.1;
   box1.rotation.y = box2.rotation.y = box3.rotation.y += 0.1;
-  sphere.rotation.x += 0.05;
-  sphere.rotation.z += 0.1;
+  sphere.rotation.y += 0.025;
   box1.position.x = 2.5 * Math.cos( time / 1500.0);
   box1.position.y = 2.5 * Math.sin( time / 1500.0);
   box2.position.x = 2.5 * Math.cos( time * 0.66 * Math.PI / 1500.0);
