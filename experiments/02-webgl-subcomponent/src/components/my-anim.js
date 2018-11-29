7bb5f0d0c4c2a7d7fc2756433e539f48fbbf1dd3
Olivier Lange <olange@petit-atelier.ch>
2018-11-29 02:27:25 +0100

    Adds two <my-anim-sub> subcomponents with rotating wireframes to the <my-anim> component

--- experiments/02-webgl-subcomponent/src/components/my-anim.js
+++ experiments/02-webgl-subcomponent/src/components/my-anim.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 
 class MyAnim extends LitElement {
   render() {
-    console.log( "render()");
+    console.log( "my-anim › render()");
     return html`
       <style>
         :host { display: flex; flex-direction: column; }
@@ -19,40 +19,43 @@ class MyAnim extends LitElement {
 
   static get properties() {
     return {
-      interval: { type: Number }
+      // Observed properties, reflected to attributes
+      interval: { type: Number, reflect: true },
+      // Observed properties, not reflected to attributes
+      scene: { type: Object, reflect: false },
+      camera: { type: Object, reflect: false },
+      renderer: { type: Object, reflect: false }
     };
   }
 
   constructor() {
-    console.log( "constructor()");
+    console.log( "my-anim › constructor()");
     super();
 
-    // Element attributes
+    // Element public properties
     this.interval = 10.0; // ms
-
-    // Element private properties
     this.camera = null;
     this.scene = null;
     this.renderer = null;
-    this.mesh = null
 
-    this.canvasElement = null;
-    this.lastTime = null;
-
+    // Element private properties
+    this._canvasElement = null;
+    this._lastTime = null;
+    this._mesh = null
     this._boundAnimateFn = this.animate.bind( this);
   }
 
   firstUpdated() {
-    console.log( "firstUpdated()");
-    this.canvasElement = this.shadowRoot.getElementById( "animation");
+    console.log( "my-anim › firstUpdated()");
+    this._canvasElement = this.shadowRoot.getElementById( "animation");
     this.init();
     this.animate();
   }
 
   init() {
-    console.log( "init()");
-    const displayWidth = this.canvasElement.clientWidth;
-    const displayHeight = this.canvasElement.clientHeight;
+    console.log( "my-anim › init()");
+    const displayWidth = this._canvasElement.clientWidth;
+    const displayHeight = this._canvasElement.clientHeight;
     const displayRatio = displayWidth / displayHeight;
 
     this.camera = new THREE.PerspectiveCamera( 70, displayRatio, 0.01, 10);
@@ -60,37 +63,37 @@ class MyAnim extends LitElement {
 
     const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2);
     const material = new THREE.MeshNormalMaterial();
-    this.mesh = new THREE.Mesh( geometry, material);
+    this._mesh = new THREE.Mesh( geometry, material);
 
     this.scene = new THREE.Scene();
-    this.scene.add( this.mesh);
+    this.scene.add( this._mesh);
 
     this.renderer = new THREE.WebGLRenderer(
-      { antialias: true, canvas: this.canvasElement });
+      { antialias: true, canvas: this._canvasElement });
   }
 
   resize() {
-    const displayWidth = this.canvasElement.clientWidth;
-    const displayHeight = this.canvasElement.clientHeight;
+    const displayWidth = this._canvasElement.clientWidth;
+    const displayHeight = this._canvasElement.clientHeight;
     const displayRatio = displayWidth / displayHeight;
-    console.log( `resize() to ${displayWidth}x${displayHeight} (1:${displayRatio})`);
+    console.log( `my-anim › resize() to ${displayWidth}x${displayHeight} (1:${displayRatio})`);
     this.camera.aspect = displayRatio;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize( displayWidth, displayHeight, false);
   }
 
   needsResize() {
-    const displayWidth = this.canvasElement.clientWidth;
-    const displayHeight = this.canvasElement.clientHeight;
+    const displayWidth = this._canvasElement.clientWidth;
+    const displayHeight = this._canvasElement.clientHeight;
     const renderSize = this.renderer.getSize();
     return( renderSize.width != displayWidth || renderSize.height != displayHeight);
   }
 
   updateScene( time) {
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
-    this.mesh.position.x = 0.5 * Math.cos( time / 1500.0);
-    this.mesh.position.y = 0.5 * Math.sin( time / 1500.0);
+    this._mesh.rotation.x += 0.01;
+    this._mesh.rotation.y += 0.02;
+    this._mesh.position.x = 0.5 * Math.cos( time / 1500.0);
+    this._mesh.position.y = 0.5 * Math.sin( time / 1500.0);
   }
 
   animate( time) {
@@ -103,9 +106,9 @@ class MyAnim extends LitElement {
 
     this.updateScene.call( this, currTime);
 
-    if( firstTime || time - this.lastTime > this.interval) {
+    if( firstTime || time - this._lastTime > this.interval) {
       this.renderer.render( this.scene, this.camera);
-      this.lastTime = currTime;
+      this._lastTime = currTime;
     }
 
     window.requestAnimationFrame( this._boundAnimateFn);
